55b2c1450b7e8773fd77b9a76ecdd89f24a64139
Pavel <magma4810@gmail.com>
2024-12-11 16:11:30 +0300

    add header and sidebar

---
+++ src/components/SideBar/MenuElement.jsx
@@ -0,0 +1,11 @@
+export function MenuElement({children}){
+    return (
+        <>
+        <a href="#">
+            <label className="menuElement">
+            <input type="checkbox" value="value" name="name"/>
+            {children}</label>
+        </a> 
+        </>
+    )
+}
